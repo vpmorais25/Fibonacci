d7f0ed67cd038913163a132f44a2e01831309473
vpmorais25 <47580635+vpmorais25@users.noreply.github.com>
2019-10-18 01:53:10 -0300

    Add files via upload

---
+++ fibonacci/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Bstart = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.MaxLength = 1000000000
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(538, 178)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TabStop = False
+        '
+        'Bstart
+        '
+        Me.Bstart.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Bstart.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bstart.Location = New System.Drawing.Point(12, 196)
+        Me.Bstart.Name = "Bstart"
+        Me.Bstart.Size = New System.Drawing.Size(538, 53)
+        Me.Bstart.TabIndex = 1
+        Me.Bstart.Text = "Next"
+        Me.Bstart.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(562, 261)
+        Me.Controls.Add(Me.Bstart)
+        Me.Controls.Add(Me.TextBox1)
+        Me.MinimumSize = New System.Drawing.Size(578, 300)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Fibonacci"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Bstart As Button
+End Class
---
+++ fibonacci/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+
+
+    Dim num As Integer
+
+
+
+    Private Sub Bstart_Click(sender As Object, e As EventArgs) Handles Bstart.Click
+        Bstart.Enabled = False
+        Fibonacci()
+    End Sub
+
+
+    Private Sub Fibonacci()
+
+        Txt(F(num))
+            num = num + 1
+            Bstart.Enabled = True
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        num = 0
+    End Sub
+
+    Private Sub Txt(txt)
+
+        If TextBox1.Text = "" Then
+            TextBox1.Text = txt
+        Else
+            TextBox1.Text = TextBox1.Text & " - " & txt
+        End If
+
+    End Sub
+
+
+    Private Function F(ByVal n As Long) As Long
+
+        Dim res As Long
+
+        Select Case n
+            Case 0
+                res = 0
+            Case 1, 2
+                res = 1
+            Case > 2
+                res = F(n - 1) + F(n - 2)
+        End Select
+
+        'If n = 0 Then
+        '    res = 0
+        'ElseIf n = 1 Or n = 2 Then
+        '    res = 1
+        'ElseIf n > 2 Then
+        '    res = F(n - 1) + F(n - 2)
+        'End If
+
+        F = res
+
+    End Function
+
+End Class
